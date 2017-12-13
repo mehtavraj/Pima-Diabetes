2cca7699a8ab64e6de9e9866558ed7de1c7246f2
mehtavraj <vrajmehta2010@live.com>
2017-12-13 14:14:17 -0800

    Loading the Dataset

--- Pima.py
+++ Pima.py
@@ -4,3 +4,7 @@ from numpy import loadtxt
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+#Loading dataset and to retrieve first 5 rows
+dataset = pd.read_csv('pima-indians-diabetes.csv')
+dataset.head()
