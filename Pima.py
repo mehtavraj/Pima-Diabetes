70c07d3a7fef1f59c720240bc23149ad343ea666
mehtavraj <vrajmehta2010@live.com>
2017-12-13 14:21:36 -0800

    Fit the model

--- Pima.py
+++ Pima.py
@@ -17,3 +17,8 @@ Y = dataset.iloc[:,8]
 seed = 7
 test_size = 0.33
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+# Creating XGBClassifier and fitting into training set of data 
+# Used Scikit-learn to fit the model
+model = XGBClassifier()
+model.fit(X_train, y_train)
