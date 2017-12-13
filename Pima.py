2d45e610df4f89d8ce31a21b8fefb5822b699385
mehtavraj <vrajmehta2010@live.com>
2017-12-13 14:12:31 -0800

    Importing all Packages including XGBoost

---
+++ Pima.py
@@ -0,0 +1,6 @@
+# import all packages
+import pandas as pd
+from numpy import loadtxt
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
